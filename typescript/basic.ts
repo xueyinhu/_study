d29ea5924e5f75c7571ecc7af240074f64fa7fb0
xueyinhu <13453894389@163.com>
2023-08-15 23:54:08 +0800

    that

---
+++ typescript/basic.ts
@@ -0,0 +1,19 @@
+// HelloWorld!
+const sayHello: string = "Hello!"
+console.log(sayHello);
+
+// 生成对应的 js 文件
+// tsc basic.ts
+
+/** 基础类型
+ * 数字
+ * 字符串
+ * 布尔
+ * 数组
+ * 元组
+ * 枚举
+ * void
+ * null
+ * undefined
+ * never
+ */
