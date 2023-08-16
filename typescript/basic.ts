92e4613072a4073ea5b0340fd85463463544b44c
xueyinhu <13453894389@163.com>
2023-08-16 23:17:54 +0800

    that

--- typescript/basic.ts
+++ typescript/basic.ts
@@ -5,15 +5,20 @@ console.log(sayHello);
 // 生成对应的 js 文件
 // tsc basic.ts
 
-/** 基础类型
- * 数字
- * 字符串
- * 布尔
- * 数组
- * 元组
- * 枚举
- * void
- * null
- * undefined
- * never
+/** 基础类型 - 关键字
+ * 任意 - any
+ *  1. 变量的值会动态改变
+ *  2. 改写现有代码，任意值允许在编译时可选择地包含或移除类型检查
+ *  3. 定义存储各种类型数据的数组
+ * 数字 - number
+ * 字符串 - string
+ * 布尔 - boolean
+ * 数组 - 无
+ * 元组 - 无
+ * 枚举 - enum
+ * void - void
+ * null - null
+ * undefined - undefined
+ * never - never
  */
+let x: any;
